d17d1c871bcb7405de80e6b1d381137a1e39631c
wang <quang07112004@gmail.com>
2023-05-25 22:57:32 +0700

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< Updated upstream
 import { HomeComponent } from './home/home.component';
 import { PlatformBaseComponent } from './platform-base/platform-base.component';
 
@@ -12,6 +13,24 @@ import { PlatformBaseComponent } from './platform-base/platform-base.component';
     HomeComponent,
     PlatformBaseComponent
   ],
+=======
+import { HomeComponent } from './components/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { GaugeModule } from 'angular-gauge';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent, NavbarComponent, FooterComponent],
+>>>>>>> Stashed changes
   imports: [
     BrowserModule,
     AppRoutingModule
