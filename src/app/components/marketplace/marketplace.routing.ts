f443993e2a23231b14fa91af0cf1c4041245e3f3
codie <ngnhathuy1224@gmail.com>
2023-06-13 16:37:20 +0700

    del

--- src/app/components/marketplace/marketplace.routing.ts
+++ src/app/components/marketplace/marketplace.routing.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
-import { ProductsComponent } from './products/products.component';
 import { MarketplaceComponent } from './marketplace/marketplace.component';
 
 const routes: Route[] = [
   {
     path: '',
     component: MarketplaceComponent,
-  },
-  {
-    path: 'product/:id',
-    component: ProductsComponent,
   },
 ];
 
