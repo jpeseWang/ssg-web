814c3ceff9f91380213a30cb45d730fc240a5b40
wang <quang07112004@gmail.com>
2023-06-28 11:19:01 +0700

    home UI

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { MatDialog } from '@angular/material/dialog';
+import { VideoComponent } from './video/video.component';
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
-  styleUrls: ['./movie.component.css']
+  styleUrls: ['./movie.component.css'],
 })
 export class MovieComponent {
-
+  constructor(private dialog: MatDialog) {}
+  openFullscreenVideo() {
+    const dialogRef = this.dialog.open(VideoComponent, {
+      width: '140%',
+      height: '90%',
+    });
+  }
 }
